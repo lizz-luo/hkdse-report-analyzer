c41d86797866bcdd6bb76f8e864680fb7886e98d
lizz-luo <lizzluo@gmail.com>
2026-05-28 15:36:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -317,7 +317,6 @@ with tab0:
                     ds_text = "\n".join(df_total["日校"].astype(str).tolist())
                     st.code(ds_text, language="text")
             st.table(df_total.style.format(precision=2))
-
             chart_df = df_total[df_total['等級'] != '出席 Sat'].copy()
             chart_long = chart_df.melt(id_vars='等級', var_name='學校', value_name='人數')
             chart = alt.Chart(chart_long).mark_bar().encode(
@@ -328,6 +327,7 @@ with tab0:
                 tooltip=['等級', '學校', '人數']
             ).properties(height=380)
             st.altair_chart(chart, use_container_width=True)
+
 
 with tab1:
     st.subheader("📝 項目分析報告轉換 | Item Analysis Converter")
